1b1b44d6a1872c0d738ae588414717cabc93f556
LeNguyenTrungNam <lenguyentrungnam22@gmail.com>
2022-03-24 00:08:35 +0700

    bigshool2

--- 1911065301_LeNguyenTrungNam_Bigschool/Controllers/CoursesController.cs
+++ 1911065301_LeNguyenTrungNam_Bigschool/Controllers/CoursesController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿
+using _1911065301_LeNguyenTrungNam_Bigschool.Models;
+using _1911065301_LeNguyenTrungNam_Bigschool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,41 @@ namespace _1911065301_LeNguyenTrungNam_Bigschool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- 1911065301_LeNguyenTrungNam_Bigschool/ViewModels/CourseViewModel.cs
+++ 1911065301_LeNguyenTrungNam_Bigschool/ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using _1911065301_LeNguyenTrungNam_Bigschool.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace _1911065301_LeNguyenTrungNam_Bigschool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date  { get; set; }
+        [Required]
         public string Time  { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
